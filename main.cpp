eb5e31e52d4b23dfe0cac164989cf031374461a6
Mak Alamin <makalamin012@gmail.com>
2021-06-14 01:17:25 +0600

    C++ Practicing starts

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int Add(int a, int b){
+    return a+b;
+}
+
+int main()
+{
+    cout << "Hello CPP World!" << endl;
+
+    cout <<  Add(5,5) << endl;
+    return 0;
+}
